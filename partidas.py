51ba4b0e990be4d40ab58c8175ec78358c8a2f24
Lucas Manoel <87917746+PIST0LINHA@users.noreply.github.com>
2026-05-28 16:12:36 -0300

    atualizacao parcial
    nao funciona ainda

--- partidas.py
+++ partidas.py
@@ -1,29 +1,37 @@
 import random
+import json
 
-lista_partidas = [];
+ARQ_LISTA_PARTIDAS = "partidas.json"
+_lista_partidas = []
+lista_partidas = []
 
-def simular_partida(time1:str, time2:str) -> dict:
+def simular_partida(time1:str, time2:str) -> int:
     if not time1 or not time2:
         return{"erro": "Erro: nomes dos timer não podem ser vazio"}
 
     if time1 == time2:
         return{"erro": "Erro: um time não pode disputar contra si mesmo"}
 
-    return {
+    partida = {
         "time1": time1,
-        "tiem2": time2,
-        "gols_time1": randint(0, 5),
-        "gols_time2": randint(0, 5)
+        "time2": time2,
+        "gols_time1": random.randint(0, 5),
+        "gols_time2": random.randint(0, 5)
     }
+    
+    _lista_partidas.append(partida)
+    return 1
 
 
-def gerar_resultado(partida:dict) -> str:
+def gerar_resultado(partida:dict) -> int:
     if "erro" in partida:
-        return partida["erro"]
-    return(
+        print(f'{partida["erro"]}')
+        return -1
+    print(
         f'{partida["time1"]}:{partida["gols_time1"]}  x'
         f'{partida["time2"]}: {partida["gols_time2"]}'
     )
+    return 1
 
 
 def registrar_resultado(lista_partidas:list, time1:str, time2:str, gols_time1:str, gols_time2:str) -> dict:
